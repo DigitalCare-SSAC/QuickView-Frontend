173c3916f37070fc01055c70d96cbaa15addd239
Carlos Galindo <utm22030587@utma.edu.mx>
2025-04-23 21:22:33 -0600

    feat: add custom image loader

---
+++ src/ts/customImageLoader.ts
@@ -0,0 +1,67 @@
+import { parseImageId } from "@cornerstonejs/dicom-image-loader/wadouri";
+import * as cornerstone from "@cornerstonejs/core";
+
+export function loadImage(imageId: string) {
+    const url = parseImageId(imageId);
+
+    const promise = new Promise((resolve, reject) => {
+        const oReq = new XMLHttpRequest();
+        oReq.open('get', url, true);
+        oReq.responseType = 'arraybuffer';
+
+        oReq.onreadystatechange = function () {
+            if (oReq.readyState === 4) {
+                if (oReq.status === 200) {
+                    // Aquí podrías parsear o decodificar el arrayBuffer real
+                    // Pero en este ejemplo simplemente simulas un objeto imagen
+                    const image = createImageObject(imageId);
+                    resolve(image);
+                } else {
+                    reject(new Error(oReq.statusText));
+                }
+            }
+        };
+
+        oReq.send();
+    });
+
+    return {
+        promise,
+        cancelFn: undefined
+    };
+}
+
+function createImageObject(imageId: string) {
+    const width = 512;
+    const height = 512;
+    const numPixels = width * height;
+    const pixelData = new Uint8Array(numPixels);
+    const rnd = Math.round(Math.random() * 255);
+
+    for (let i = 0; i < numPixels; i++) {
+        pixelData[i] = (i % width + rnd) % 256;
+    }
+
+    const getPixelData = () => pixelData;
+
+    return {
+        imageId,
+        minPixelValue: 0,
+        maxPixelValue: 255,
+        slope: 1.0,
+        intercept: 0,
+        windowCenter: 127,
+        windowWidth: 256,
+        getPixelData,
+        render: cornerstone.renderGrayscaleImage, // Requerido por Cornerstone
+        rows: height,
+        columns: width,
+        height,
+        width,
+        color: false,
+        columnPixelSpacing: 1.0,
+        rowPixelSpacing: 1.0,
+        invert: false,
+        sizeInBytes: numPixels
+    };
+}
